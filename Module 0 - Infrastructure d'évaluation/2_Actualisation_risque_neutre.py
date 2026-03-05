669701f541fa6d84e8a27211258c091b50b40f09
Ianis Le Berre <ianisleberre@yahoo.fr>
2026-03-05 01:10:27 +0100

    Modification

--- Module 0 - Infrastructure d'évaluation/2_Actualisation_risque_neutre.py
+++ Module 0 - Infrastructure d'évaluation/2_Actualisation_risque_neutre.py
@@ -18,21 +18,58 @@ Les évèvenements intraday sont considérés comme non pertinents, car cela sup
 '''
 
 
+from tracemalloc import start
+
 import numpy as np
 import pandas as pd
 import datetime as dt
 
 from pylab import mpl, plt
-plt.style.use('seaborn')
+plt.style.use('seaborn-v0_8')
 mpl.rcParams['font.family'] = 'serif'
 
 import sys
 sys.path.append('../dx')
 
 
-# Définition dates 
+# Les 2 définition dates et fractions d'année sont équivalentes 
 dates = [dt.datetime(2020,1,1), dt.datetime(2020,7,1), dt.datetime(2021,1,1)]
+print("Dates 1")
+print((dates[1] - dates[0]).days / 365.0)
+print("\n Dates 2")
+print((dates[2] - dates[1]).days / 365.0)
 
-# Définition fractions d'année
-fractions = (dates[1] - dates[0]).days / 365.0 
+fractions = [0.0, 0.5, 1.0]
 
+'''
+L'équivalence est imparfaite car une fraction d'année tombe rarement sur le même début qu'une date exprimée explicitement. (0 heure le premier jour)
+On ne peut pas diviser le nombre de jours d'une année par 50. 
+
+Nous allons créer une fonction get_year_deltas() afin de générer des fractions d'année à partir d'une liste de dates.
+'''
+
+def get_year_deltas(date_list, day_count=365.):
+    ''' Return vector of floats with day deltas in year fractions.
+    Initial value normalized to zero.
+
+    Parameters
+    ==========
+    date_list: list or array
+    collection of datetime objects
+    day_count: float
+    number of days for a year
+    (to account for different conventions)
+
+    Results
+    =======
+    delta_list: array
+    year fractions
+    '''
+
+    start = date_list[0]
+    delta_list = [(date - start).days / day_count for date in date_list]
+    return np.array(delta_list)
+
+# La fonction s'utilise de la manière suivante : 
+print("\n Fonction get_year_deltas()")
+print(get_year_deltas(dates))
